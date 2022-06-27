918d3bdf2f4005b84356595d439d0f1e8b5d6a9c
lenh0205 <lenhdeptrai98@gmail.com>
2022-06-27 11:20:43 +0700

    update songUrl

--- generate-data.js
+++ generate-data.js
@@ -64,7 +64,10 @@ const randomSongList = (playlistList, numberOfSongs) => {
                 songUrl: faker.helpers.arrayElement([
                     'https://vnso-zn-16-tf-mp3-s1-zmp3.zmdcdn.me/397dfae855a9bcf7e5b8/3316510322627274731?authen=exp=1656396063~acl=/397dfae855a9bcf7e5b8/*~hmac=802ff53522f3b10ae9790b2b53e6286a&fs=MTY1NjIyMzI2Mzg4NXx3ZWJWNnwxMDUwNjU5MjQ0fDExOC43MC4xNC4yNDA',
                     'https://vnso-zn-23-tf-mp3-s1-zmp3.zmdcdn.me/1c5fc9a67ae793b9caf6/8696983054732805962?authen=exp=1656396393~acl=/1c5fc9a67ae793b9caf6/*~hmac=7acd454ea557bb6b16d4ab00d17bcebe&fs=MTY1NjIyMzU5MzI1OXx3ZWJWNnwwfDE0LjE2NS4yMjmUsICdUngMjM1',
-                    'https://vnso-zn-5-tf-mp3-s1-zmp3.zmdcdn.me/a0f02cfa7dbb94e5cdaa/5159577349354650802?authen=exp=1656396623~acl=/a0f02cfa7dbb94e5cdaa/*~hmac=aca46392c7ee97c9400be78e39cfddeb&fs=MTY1NjIyMzgyMzY0N3x3ZWJWNnwwfDE0LjE2NC4yMjUdUngNzI'
+                    'https://vnso-zn-5-tf-mp3-s1-zmp3.zmdcdn.me/a0f02cfa7dbb94e5cdaa/5159577349354650802?authen=exp=1656396623~acl=/a0f02cfa7dbb94e5cdaa/*~hmac=aca46392c7ee97c9400be78e39cfddeb&fs=MTY1NjIyMzgyMzY0N3x3ZWJWNnwwfDE0LjE2NC4yMjUdUngNzI',
+                    'https://vnso-zn-5-tf-mp3-s1-zmp3.zmdcdn.me/3f23f3549e15774b2e04/3291725272894183767?authen=exp=1656476470~acl=/3f23f3549e15774b2e04/*~hmac=093b1c95a85fd886e9b432f04c17049b&fs=MTY1NjMwMzY3MDg0N3x3ZWJWNnwwfDE0LjE2NC4yMjUdUngNzI',
+                    'https://vnso-zn-23-tf-mp3-s1-zmp3.zmdcdn.me/fb400d3760768928d067/5668674868526934830?authen=exp=1656476455~acl=/fb400d3760768928d067/*~hmac=aebafed649c395e81e1c449d85981826&fs=MTY1NjMwMzY1NTgwNnx3ZWJWNnwwfDE0LjE2NC4yMjUdUngNzI',
+                    'https://vnso-zn-16-tf-mp3-s1-zmp3.zmdcdn.me/6660bd17d05639086047/6500702918192854609?authen=exp=1656475957~acl=/6660bd17d05639086047/*~hmac=3e72ae977ab9d965240d7805f3af0841&fs=MTY1NjMwMzE1NzM1Nnx3ZWJWNnwwfDU4LjE4Ny4xNjMdUngMjI'
                 ]),
                 createdAt: Date.now(),
                 updatedAt: Date.now()
